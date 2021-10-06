bbdb315090ff7f3d32983109337518822af34bd9
Puspita Sari Matovanni <vaynnipuspita29@gmail.com>
2021-10-06 11:47:18 +0700

    add print in client TCP

--- client.py
+++ client.py
@@ -1,9 +1,24 @@
 from socket import *
 serverAddress = 'localhost'
 serverPort = 12000
-with socket(AF_INET, SOCK_STREAM) as clientSocket:
+clientSocket  = socket(AF_INET, SOCK_STREAM)
+try:
     clientSocket.connect((serverAddress,serverPort))
-   
-   #TODO
-   
+    
+    username = input("Enter the username: \n")
+    clientSocket.send(username.encode())
+
+    result = clientSocket.recv(1024)
+    print("Response: " + result.decode())
+    print("Hello.! Welcome to the chatroom.\n instructions:")
+    print("1. Simply type the message to send broadcast to all active clients\n2. Type @username<space>yourmessage' without quotes to send message to desired client")
+    print("3. Type 'WHOISIN' without quotes to see list of active clients\n4. Type 'LOGOUT without quotes to logoff server")  
+
+    print(result.decode())
     clientSocket.close()
+
+except:
+    print("Connection has been lost")
+    clientSocket.close()
+
+    
